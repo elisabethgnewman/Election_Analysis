6e93497f978c09fdfa54e71b4ff9d5f84d49c563
elisabethgnewman <88783255+elisabethgnewman@users.noreply.github.com>
2021-09-11 10:14:34 -0500

    Adding the election audit code.

--- Python_practice.py
+++ Python_practice.py
@@ -1,1 +1,5 @@
 print("Hello World")
+counties = ["Arapahoe","Denver","Jefferson"]
+print(counties)
+if counties[1] == 'Denver':
+    print(counties[1])
---
+++ Python_practice2.py
@@ -0,0 +1,30 @@
+counties = ["Arapahoe","Denver","Jefferson"]
+if counties[1] == 'Denver': 
+    print(counties[1])
+if "El Paso" in counties:
+    print("El Paso is in the list of counties")
+else:
+    print("El Paso is not in the list of counties")
+if "Arapahoe" in counties and "El Paso" in counties:
+    print("Arapahoe and El Paso are in the list of counties.")
+else:
+    print("Arapahoe or El Paso is not in the list of counties.")
+for county in counties:
+    print(county)
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for voters in counties_dict.values():
+    print(voters)
+for county in counties_dict.values():
+    print(counties_dict.get(county))
+temperature = int(input("What is the temperature outside?"))
+if temperature > 80:
+    print("Turn on the Ac")
+else:
+    print("Open the window")
+numbers = [0, 1, 2, 3, 4]
+for num in numbers:
+    print(num)
+for num in range(5):
+    print(num)
+for i in range(len(counties)):
+    print(counties[i])
